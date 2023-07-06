17910a8bdbb73f5440dde116ace1353035586ebb
chenyan <chenyan@zhitongits.com.cn>
2023-07-06 09:45:59 +0800

    [160]相交链表优化

--- linked-list-easy/160.相交链表.js
+++ linked-list-easy/160.相交链表.js
@@ -19,34 +19,11 @@
  * @return {ListNode}
  */
 
-/**
- * 链表节点
- * @param {*} val
- * @param {ListNode} next
- */
-function ListNode(val, next = null) {
-  this.val = val;
-  this.next = next;
-}
-/**
- * 将一个数组转为链表
- * @param {array} array
- * @return {ListNode}
- */
-const getListFromArray = (array) => {
-  let dummy = new ListNode();
-  let pre = dummy;
-  array.forEach((x) => (pre = pre.next = new ListNode(x)));
-  return dummy.next;
-};
-
 var getIntersectionNode = function (headA, headB) {
   let lenA = 0,
     lenB = 0;
   let curA = headA,
     curB = headB;
-
-  let sub = 0;
 
   while (curA) {
     lenA++;
@@ -58,13 +35,18 @@ var getIntersectionNode = function (headA, headB) {
     curB = curB.next;
   }
 
-  sub = Math.abs(lenA - lenB);
   curA = headA;
   curB = headB;
 
-  while (sub--) {
-    if (lenA < lenB) curB = curB.next;
-    if (lenA > lenB) curA = curA.next;
+  if (lenA < lenB) {
+    [lenA, lenB] = [lenB, lenA];
+    [curA, curB] = [curB, curA];
+  }
+
+  let n = lenA - lenB;
+
+  while (n--) {
+    curA = curA.next;
   }
 
   while (curA) {
@@ -76,5 +58,4 @@ var getIntersectionNode = function (headA, headB) {
   return null;
 };
 
-// console.log(getIntersectionNode([4, 1, 8, 4, 5], [5, 6, 1, 8, 4, 5]));
 // @lc code=end
