ab399683d4ab2f94d060ab184c56018ddfcd55a9
chenyan <chenyan@zhitongits.com.cn>
2023-07-12 17:37:33 +0800

    [344][two-pointers]反转字符串

---
+++ two-pointers-easy/344.反转字符串.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=javascript
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let l = 0;
+  let r = s.length - 1;
+  while (l < r) {
+    let temp = s[r];
+    s[r--] = s[l];
+    s[l++] = temp;
+  }
+  return s;
+};
+// @lc code=end
