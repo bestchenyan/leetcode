71918a65c5a04b01a8a0e9276d093dd8d08be59d
chenyan <chenyan@zhitongits.com.cn>
2023-12-19 08:49:07 +0800

    [34]排序数组中查找元素的第一和最后一个位置

--- binary-search-medium/34.在排序数组中查找元素的第一个和最后一个位置.js
+++ binary-search-medium/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -11,47 +11,57 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  const leftBoundary = getLeftBoundary(nums, target);
-  const rightBoundary = getRightBoundary(nums, target);
-  if (leftBoundary === -2 || rightBoundary === -2) return [-1, -1];
-  if (rightBoundary - leftBoundary > 1)
-    return [leftBoundary + 1, rightBoundary - 1];
+  const left = findLeftBoundary(nums, target);
+  const right = findRightBoundary(nums, target);
+  if (left == -2 || right == -2) return [-1, -1];
+  if (right - left > 1) return [left + 1, right - 1];
   return [-1, -1];
 };
 
-function getLeftBoundary(nums, target) {
-  let start = 0,
-    mid = 0,
-    end = nums.length - 1,
-    leftBoundary = -2;
+function findLeftBoundary(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  // 初始值为-2，避免target在num[0]的位置
+  let leftBoundary = -2;
   while (start <= end) {
-    mid = Math.floor((start + end) / 2);
-
+    const mid = Math.floor((start + end) / 2);
+    // end值更新时，计算左边界
     if (nums[mid] >= target) {
       end = mid - 1;
-      leftBoundary = end;
-    } else {
+      // 当mid为0时，仍旧没找到target，则target不在数组中，左边界为初始值-2
+      if (mid == 0 && nums[mid] > target) {
+        leftBoundary = -2;
+      } else {
+        leftBoundary = end;
+      }
+    } else if (nums[mid] < target) {
       start = mid + 1;
     }
   }
   return leftBoundary;
 }
 
-function getRightBoundary(nums, target) {
-  let start = 0,
-    mid = 0,
-    end = nums.length - 1,
-    rightBoundary = -2;
+function findRightBoundary(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  // 初始值为-2，避免target在num[0]的位置
+  let rightBoundary = -2;
   while (start <= end) {
-    mid = Math.floor((start + end) / 2);
-
+    const mid = Math.floor((start + end) / 2);
+    // start值更新时，计算右边界
     if (nums[mid] <= target) {
       start = mid + 1;
-      rightBoundary = start;
-    } else {
+      // 当mid为最后一位时，仍旧没找到target，则target不在数组中，右边界为初始值-2
+      if (mid == nums.length - 1 && nums[mid] < target) {
+        rightBoundary = -2;
+      } else {
+        rightBoundary = start;
+      }
+    } else if (nums[mid] > target) {
       end = mid - 1;
     }
   }
   return rightBoundary;
 }
+
 // @lc code=end
