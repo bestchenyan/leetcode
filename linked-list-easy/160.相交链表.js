68dab531ed9b1a2d71615cfcd3dbb272ac5ca76b
chenyan <chenyan@zhitongits.com.cn>
2024-01-16 08:31:08 +0800

    [160]链表相交

--- linked-list-easy/160.相交链表.js
+++ linked-list-easy/160.相交链表.js
@@ -20,42 +20,15 @@
  */
 
 var getIntersectionNode = function (headA, headB) {
-  let lenA = 0,
-    lenB = 0;
-  let curA = headA,
-    curB = headB;
+  let a = headA,
+    b = headB;
 
-  while (curA) {
-    lenA++;
-    curA = curA.next;
+  while (a != b) {
+    a = !a ? headB : a.next;
+    b = !b ? headA : b.next;
   }
 
-  while (curB) {
-    lenB++;
-    curB = curB.next;
-  }
-
-  curA = headA;
-  curB = headB;
-
-  if (lenA < lenB) {
-    [lenA, lenB] = [lenB, lenA];
-    [curA, curB] = [curB, curA];
-  }
-
-  let n = lenA - lenB;
-
-  while (n--) {
-    curA = curA.next;
-  }
-
-  while (curA) {
-    if (curA == curB) return curA;
-    curA = curA.next;
-    curB = curB.next;
-  }
-
-  return null;
+  return a;
 };
 
 // @lc code=end
