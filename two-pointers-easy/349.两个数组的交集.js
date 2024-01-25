1bbfa429e0871054eda50fff67017a74560a22a8
chenyan <chenyan@zhitongits.com.cn>
2024-01-25 08:08:35 +0800

    [349]两个数组的交集

---
+++ two-pointers-easy/349.两个数组的交集.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  const set1 = new Set([...nums1]);
+  return [...new Set(nums2.filter((num) => set1.has(num)))];
+};
+// @lc code=end
