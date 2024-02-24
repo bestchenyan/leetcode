0b6dfd7b0610cc119b1af9c078d89ee59145dfa1
chenyan <chenyan@zhitongits.com.cn>
2024-02-24 11:51:29 +0800

    [107]二叉树的层序遍历II

---
+++ tree-medium/107.二叉树的层序遍历-ii.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=javascript
+ *
+ * [107] 二叉树的层序遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function (root) {
+  const queue = [],
+    result = [];
+  if (root != null) {
+    queue.push(root);
+  }
+
+  while (queue.length) {
+    const size = queue.length;
+    const res = [];
+
+    for (let k = 0; k < size; k++) {
+      const node = queue.shift();
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      res.push(node.val);
+    }
+
+    result.push(res);
+  }
+
+  return result.reverse();
+};
+// @lc code=end
