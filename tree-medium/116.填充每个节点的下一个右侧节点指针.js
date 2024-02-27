9227416db830fe7691e78fef064e99a5b09f005e
chenyan <chenyan@zhitongits.com.cn>
2024-02-27 08:24:05 +0800

    [116]填充每个节点的下一个右侧节点指针

---
+++ tree-medium/116.填充每个节点的下一个右侧节点指针.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=javascript
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  const queue = [];
+  if (root) {
+    queue.push(root);
+  }
+
+  while (queue.length) {
+    const size = queue.length;
+    let node, preNode;
+    for (let k = 0; k < size; k++) {
+      if (k == 0) {
+        preNode = queue.shift();
+        node = preNode;
+      } else {
+        node = queue.shift();
+        preNode.next = node;
+        preNode = preNode.next;
+      }
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  return root;
+};
+// @lc code=end
