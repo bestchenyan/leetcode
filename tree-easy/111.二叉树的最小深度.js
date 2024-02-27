553901feed0146b6d739e24ca1dcd96a59796279
chenyan <chenyan@zhitongits.com.cn>
2024-02-27 09:13:01 +0800

    [111]二叉树的最小深度

---
+++ tree-easy/111.二叉树的最小深度.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=javascript
+ *
+ * [111] 二叉树的最小深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+  const queue = [];
+  let depth = 0;
+
+  if (root) {
+    queue.push(root);
+    depth += 1;
+  }
+
+  while (queue.length) {
+    const size = queue.length;
+    for (let k = 0; k < size; k++) {
+      const node = queue.shift();
+
+      if (!node.left && !node.right) {
+        return depth;
+      } else if (k == size - 1) {
+        depth += 1;
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  return depth;
+};
+// @lc code=end
